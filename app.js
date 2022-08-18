ff56a48ca20890af8fa49f57023fc91d6c46c711
HelioSchneiderr <helioschndr@gmail.com>
2022-08-18 17:13:11 -0300

    Version 1.01
    Header addition

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars")
+
+
+
+//Configs
+
+    //Handlebars
+         const handle = handlebars.create({
+            defaultLayout: 'main'
+            });
+        
+            app.engine('handlebars', handle.engine);
+            app.set('view engine', 'handlebars');
+            app.use(express.static("public"))
+
+
+//Rotas
+    app.get("/", (req, res)=>{
+        res.render("index", {style: "style.css"})
+    })
+
+const PORT = 65
+app.listen(PORT, ()=>{
+    console.log("Server Open")
+})
+    
