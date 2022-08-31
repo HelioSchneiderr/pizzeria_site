de9b76cadcf10b76d75930030d9c43f9a9f49ee1
HelioSchneiderr <helioschndr@gmail.com>
2022-08-31 19:30:03 -0300

    Version - 1.05

--- routes/header.js
+++ routes/header.js
@@ -8,4 +8,12 @@ router.get("/faca-seu-pedido", (req, res)=>{
     res.render("header/faca-seu-pedido", {style: "style.css"})
 })
 
+router.get("/cardapio", (req, res)=>{
+    res.render("header/cardapio", {style: "style.css"})
+})
+
+router.get("/unidades", (req, res)=>{
+    res.render("header/unidades", {style: "style.css"})
+})
+
 module.exports = router
